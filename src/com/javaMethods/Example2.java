c3af804a6f1446dd4583c67efbbfac81d122cbdf
Your Name <you@example.com>
2024-07-06 10:22:12 +0530

    Initial commit

---
+++ src/com/javaMethods/Example2.java
@@ -0,0 +1,18 @@
+package com.javaMethods;
+
+public class Example2
+{
+
+	/**
+	 * 
+	 * main(): the java application/program execution starts from main() method.
+	 */
+	//main main()
+	public static void main(String[] args) 
+	{	
+		//2 instructions
+		String name = "Kiran Raut";//variable definition	
+		System.out.println(name);//print statement
+	}
+
+}
---
+++ src/com/javaTypesOfMethod/Example1.java
@@ -0,0 +1,28 @@
+package com.javaTypesOfMethod;
+
+public class Example1 
+{
+
+	//user defined method
+		public static int sumOfNumbers(int a, int b, int c)
+		{
+			return a+b+c; 
+		}
+
+		public static void main(String[] args) {
+
+
+			String text = "Welcome to Java Programming";
+
+			System.out.println(text.length());
+
+			System.out.println(sumOfNumbers(89, 34, 56));
+
+			int sum = sumOfNumbers(890, 304, 506);
+
+			System.out.println(sum);		
+
+		}
+
+	}
+
